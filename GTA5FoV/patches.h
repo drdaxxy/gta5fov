292c2b916c807f03072c5ca03a786593b582a75f
Niklas K. <drdaxxy@googlemail.com>
2015-04-18 01:58:24 +0200

    add car fov

--- GTA5FoV/patches.h
+++ GTA5FoV/patches.h
@@ -30,6 +30,8 @@ typedef struct patch_t
 	size_t paramCount;
 } patch_t;
 
+
+
 char firstPersonWalkingSignature[] =
 {
 	0xF3, 0x44, 0x0F, 0x11, 0x4D, 0x00, 0x8B, 0x86, 0xB0, 0x13, 0x00, 0x00, 0xC1, 0xE8, 0x1E
@@ -54,6 +56,32 @@ patch_parameter_t firstPersonWalkingFovParams[] =
 		4
 	}
 };
+
+char firstPersonCarSignature[] =
+{
+	0x48, 0x8B, 0xCB, 0xF3, 0x0F, 0x11, 0x83, 0x90, 0x00, 0x00, 0x00, 0x48, 0x83, 0xC4, 0x20, 0x5B
+};
+char firstPersonCarCode[] =
+{
+	0xC7, 0x83, 0x90, 0x00, 0x00, 0x00,							// mov [rbp+00],
+	0x00, 0x00, 0x88, 0x42,										// (fov - default 68)
+	0x48, 0x83, 0xC4, 0x20,										// add rsp, 20 (copied over)
+	0x5B,														// pop rbx (copied over)
+	0x49, 0xBB,													// mov r11, 
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,				// (return jump address)
+	0x41, 0xFF, 0xE3											// jmp r11
+};
+patch_parameter_t firstPersonCarFovParams[] =
+{
+	{
+		"fov",
+		"FirstPerson",
+		"Car",
+		6,
+		4
+	}
+};
+
 patch_t fovPatches[] =
 {
 	{
@@ -67,6 +95,18 @@ patch_t fovPatches[] =
 		18,
 		firstPersonWalkingFovParams,
 		1
+	},
+	{
+		"firstpersoncar",
+		firstPersonCarSignature,
+		16,
+		3,
+		0,
+		firstPersonCarCode,
+		28,
+		17,
+		firstPersonCarFovParams,
+		1
 	}
 };
-size_t fovPatchCount = 1;
+size_t fovPatchCount = 2;
